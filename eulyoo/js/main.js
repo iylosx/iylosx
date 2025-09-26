55479dd4b0a577e647b204f807669f3a5a9154e9
iylosx <plzsirrla@gmail.com>
2025-09-26 17:22:34 +0900

    250926 을유문화사

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -78,5 +78,30 @@ $(document).ready(function(){
         },
     });
 
+    const archive_swiper = new Swiper('.archive .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 15, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+    
+        navigation: {
+            nextEl: '.archive .ctrl_btn .btn_next',
+            prevEl: '.archive .ctrl_btn .btn_prev',
+        },
+       
+    });
+    $('footer .top').on('click', function(){
+        // console.log('연결')
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+
+    })
+
 
 }) /// 맨끝 !!
