f3c87f30a81bec0df4413b2665e08f6d2ef4a822
iylosx <plzsirrla@gmail.com>
2025-10-28 15:50:47 +0900

    1028 금호건설 메인 끝
    서브페이지 나갈 것

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -103,9 +103,27 @@ $(document).ready(function(){
             
         })
 
-    
+/***********
+ * footer .family_site !!!!!!!!!
+ * ***************/
 
+        $('footer .f_util .family_site .family_open').on('click', function(){
+            $('footer .family_site').addClass('open')
+            $('footer .f_util .family_site .family_wrap').slideDown()
+        })
+        $('footer .f_util .family_site .family_close').on('click', function(){
+            $('footer .family_site').removeClass('open')
+            $('footer .f_util .family_site .family_wrap').slideUp()
+        })
 
+/******************
+ * top버튼 기능
+ * ****************** */
 
+        $('footer .f_util .top').on('click', function(){
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500)
+        })
         
 })//맨끝
