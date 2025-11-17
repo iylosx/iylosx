8235236f74358042e04d352434a0509ff6ce8990
iylosx <plzsirrla@gmail.com>
2025-11-17 16:23:40 +0900

    251117 거제

--- geoje/js/main.js
+++ geoje/js/main.js
@@ -126,4 +126,22 @@ $(document).ready(function(){
         setTimeout(syncMapToSlide, 100); 
     });
     
+
+    const footer_swiper = new Swiper('footer .f_button .list .banner .banner_list .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: 'footer .f_button .list .banner .ctrl_btn .next',
+            prevEl: 'footer .f_button .list .banner .ctrl_btn .prev',
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });
+
 }) // js end
