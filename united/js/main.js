779a23939704d33d0769f7e3aa1ed3c2b31375e3
iylosx <plzsirrla@gmail.com>
2025-11-25 23:46:53 +0900

    251125 한국유나이티드제약
    제품 완료 / 반응형 들어가야함

--- united/js/main.js
+++ united/js/main.js
@@ -54,4 +54,25 @@ $('.partners .partner_wrap .list li a').on('mouseleave', function() {
 	}
 });
 
+const product_swiper = new Swiper('.product .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+	breakpoints: {
+		640: {    /* 640px 이상일때 적용 */
+			slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			spaceBetween: 20,
+		},
+	},
+	centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	
+	navigation: {
+		nextEl: '.product .ctrl_btn .next',
+		prevEl: '.product .ctrl_btn .prev',
+	},
+	
+});
+
+
+
 })
